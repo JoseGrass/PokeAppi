7acfd88da3f0eea8d4779f5bd50ec5ce67c1c388
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-04-04 23:00:55 -0500

    cambio8

--- js/lista.js
+++ js/lista.js
@@ -12,7 +12,7 @@ function mostrarLista(listaPokes){
     buscador.type = "text"
     buscador.addEventListener("input", buscarPoke);
 */
-    for (let i = 0; i < listaPokes.length; i++) {
+for (let pokemon of listaPokes) {
         let id = listaPokes[i].url.split("/")[6];
         Pokes += `
         <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${listaPokes[i].name}')">
